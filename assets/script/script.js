ae93364a9449307a51b40dbbd287fa46dd7af43f
MM-2103 <mohsen.menem1@gmail.com>
2023-11-02 12:07:09 +0100

    added frontend

---
+++ assets/script/script.js
@@ -0,0 +1,44 @@
+$(document).ready(function() {
+    $('.faq-item').on('click', function() {
+        $(this).find('b').toggle();
+        $(this).find('i').toggleClass('fa-angle-down, fa-angle-up');
+    });
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    
+  });
